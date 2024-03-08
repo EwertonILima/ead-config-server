d991d7209c336b794b99825fa3778e544da67dc0
ewertonilima <ewertoninaciolima@gmail.com>
2024-03-08 11:22:07 -0300

    🎉 Initial project
    ➕ Add config server dependency.
    ➕ Add netflix eureka client dependency.

---
+++ src/test/java/com/ewertonilima/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewertonilima.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
